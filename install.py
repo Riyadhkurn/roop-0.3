a5e9606c73fd45b15f226ea2071f4a79f233de0d
ncpt <57245077+NoCrypt@users.noreply.github.com>
2023-06-30 21:05:53 +0700

    Just install it once god damn it
    Signed-off-by: ncpt <57245077+NoCrypt@users.noreply.github.com>

--- install.py
+++ install.py
@@ -1,9 +1,13 @@
-import launch
+from __future__ import annotations
+
 import os
-import pkg_resources
 import sys
 from tqdm import tqdm
 import urllib.request
+import importlib.util
+import subprocess
+from importlib.metadata import version  # python >= 3.8
+from packaging.version import parse
 
 req_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.txt")
 
@@ -18,37 +22,73 @@ def download(url, path):
     with tqdm(total=total, desc='Downloading', unit='B', unit_scale=True, unit_divisor=1024) as progress:
         urllib.request.urlretrieve(url, path, reporthook=lambda count, block_size, total_size: progress.update(block_size))
 
+def is_installed(
+    package: str, min_version: str | None = None, max_version: str | None = None
+):
+    try:
+        spec = importlib.util.find_spec(package)
+    except ModuleNotFoundError:
+        return False
+
+    if spec is None:
+        return False
+
+    if not min_version and not max_version:
+        return True
+
+    if not min_version:
+        min_version = "0.0.0"
+    if not max_version:
+        max_version = "99999999.99999999.99999999"
+
+    if package == "google.protobuf":
+        package = "protobuf"
+
+    try:
+        pkg_version = version(package)
+        return parse(min_version) <= parse(pkg_version) <= parse(max_version)
+    except Exception:
+        return False
+
+def run_pip(*args):
+    subprocess.run([sys.executable, "-m", "pip", "install", *args])
+
+def install():
+    deps = [
+        ("insightface", "0.7.3", None),
+        ("onnx", "1.14.0", None),
+        ("onnxruntime", "1.15.0", None),
+        ("opencv-python", "4.7.0.72", None),
+        ("cython", None, None),
+    ]
+
+    for pkg, low, high in deps:
+        if not is_installed(pkg, low, high):
+            if low and high:
+                cmd = f"{pkg}>={low},<={high}"
+            elif low:
+                cmd = f"{pkg}>={low}"
+            else:
+                cmd = pkg
+
+            run_pip("-U", cmd)
+
+
 if not os.path.exists(models_dir):
     os.makedirs(models_dir)
 
 if not os.path.exists(model_path):
     download(model_url, model_path)
+    
+try:
+    import launch
+    
+    skip_install = launch.args.skip_install
+except Exception:
+    skip_install = False
 
-print("Checking roop requirements")
-with open(req_file) as file:
-    for package in file:
-        try:
-            python = sys.executable
-            package = package.strip()
+if not skip_install:
+    install()
 
-            if not launch.is_installed(package):
-                print(f"Install {package}")
-                launch.run_pip(
-                    f"install {package}", f"sd-webui-roop requirement: {package}"
-                )
-            elif "==" in package:
-                package_name, package_version = package.split("==")
-                installed_version = pkg_resources.get_distribution(package_name).version
-                if installed_version != package_version:
-                    print(
-                        f"Install {package}, {installed_version} vs {package_version}"
-                    )
-                    launch.run_pip(
-                        f"install {package}",
-                        f"sd-webui-roop requirement: changing {package_name} version from {installed_version} to {package_version}",
-                    )
 
-        except Exception as e:
-            print(e)
-            print(f"Warning: Failed to install {package}, roop will not work.")
-            raise e
+
